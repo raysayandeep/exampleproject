1064ec7b1c2ad6c9bf395975c4b2c665cc73ed84
raysayandeep <30775396+raysayandeep@users.noreply.github.com>
2022-10-18 09:55:23 +0530

    Add files via upload
    tst

---
+++ test/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {Routes,Route} from 'react-router-dom'
+import Home from './components/home';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path = "/" element = {<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App
